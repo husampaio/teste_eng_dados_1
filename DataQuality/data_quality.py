3300309247bb47f3124ebbe49246903d3a6e589e
Bruno Melo <brunomelo1315@gmail.com>
2025-04-19 20:33:16 -0300

    :sparkles: add Data Quality

---
+++ DataQuality/data_quality.py
@@ -0,0 +1,45 @@
+from typing import List
+from pyspark.sql import SparkSession
+from utils.utils import Utils
+from pyspark.sql import DataFrame
+import logging
+
+
+class DataQuality(Utils):
+    def __init__(
+        self,
+        df_to_test: DataFrame,
+        not_null_cols: List[str] = None,
+        not_negative_cols: List[str] = None,
+        unique_values_cols: List[str] = None,
+    ):
+        self.df_to_test = df_to_test
+        self.not_null_cols = not_null_cols
+        self.not_negative_cols = not_negative_cols
+        self.unique_values_cols = unique_values_cols
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO)
+
+    def run(self):
+
+        if self.not_null_cols:
+            self.check_null(self.df_to_test, self.not_null_cols)
+        if self.not_negative_cols:
+            self.check_negative(self.df_to_test, self.not_negative_cols)
+        if self.unique_values_cols:
+            self.check_unique(self.df_to_test, self.unique_values_cols)
+
+        self.logger.info("Data quality success!")
+
+
+if __name__ == "__main__":
+    spark: SparkSession = SparkSession.builder.appName("data_quality").getOrCreate()
+
+    df_to_test = spark.read.parquet("silver_layer/clients")
+
+    DataQuality(
+        df_to_test=df_to_test,
+        not_null_cols=["cod_cliente", "tp_pessoa"],
+        not_negative_cols=["vl_renda"],
+        unique_values_cols=["cod_cliente"],
+    ).run()
---
+++ DataQuality/utils/utils.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+from typing import List
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as F
+from exceptions.exceptions import NegativeValueError, NullValueError, UniqueError
+
+
+@dataclass
+class Utils:
+    @staticmethod
+    def check_null(df: DataFrame, cols: List[str]) -> DataFrame:
+        """
+        Check if the DataFrame has null values on specified columns
+
+        Parameters:
+            df (DataFrame):
+                Spark DataFrame
+            cols (List[str]):
+                Column names
+
+        :raises NullValueError: if column has null values
+        """
+        for col in cols:
+            df_test = df.filter(F.col(col).isNull())
+            if not df_test.isEmpty():
+                raise NullValueError(f"Column {col} has null values")
+
+    @staticmethod
+    def check_negative(df: DataFrame, cols: List[str]) -> DataFrame:
+        """
+        Check if the DataFrame has negative values on specified columns
+
+        Parameters:
+            df (DataFrame):
+                Spark DataFrame
+            cols (List[str]):
+                Column names
+
+        :raises NegativeValueError: if column has negative values
+        """
+        for col in cols:
+            df_test = df.filter(F.col(col) < F.lit(0))
+            if not df_test.isEmpty():
+                raise NegativeValueError(f"Column {col} has negative values")
+
+    @staticmethod
+    def check_unique(df: DataFrame, cols: List[str]) -> DataFrame:
+        """
+        Check if the DataFrame has duplicated values on specified columns
+
+        Parameters:
+            df (DataFrame):
+                Spark DataFrame
+            cols (List[str]):
+                Column names
+
+        :raises UniqueError: if column has duplicates values
+        """
+        for col in cols:
+            df_test = df.groupBy(col).agg(F.count(col).alias("qty"))
+            df_test = df_test.filter(F.col("qty") > F.lit(1))
+            if not df_test.isEmpty():
+                raise UniqueError(f"Column {col} has duplicate values")
